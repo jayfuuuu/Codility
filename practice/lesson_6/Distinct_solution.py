8dd39f347db08e1435cb4345023752c6d6045ec4
jay.fu <jay.fu@jkos.com>
2021-11-30 21:20:12 +0800

    Update Distinct solution

--- practice/lesson_6/Distinct_solution.py
+++ practice/lesson_6/Distinct_solution.py
@@ -1,11 +1,9 @@
-# https://app.codility.com/demo/results/training5N85FS-GJ4/
+# https://app.codility.com/demo/results/trainingPURU8U-DP4/
 def solution(array):
-    time = 0
     set_array = set()
     for val in array:
-        if val not in set_array : 
-            time += 1
+        if val not in set_array :
             set_array.add(val)
-    return time
+    return len(set_array)
 if __name__ == '__main__':
     print(solution([0]))
